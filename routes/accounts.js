d60bb452efce5bcc831811d5d7fc2860484a0492
sewmanudayangana <sewmaniudayangana@gmail.com>
2022-09-11 20:50:39 +0530

    update

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const router=express.Router()
-const Account=require('../models/createAccount.model')
+const Account=require('../models/accounts.models')
 router.get('/',async(req,res)=>{
     try {
         const account=await Account.find()
