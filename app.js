36d5508dfa44af50830cc85e7f608e4576217a97
sewmanudayangana <sewmaniudayangana@gmail.com>
2022-09-11 20:44:27 +0530

    init fb clone

---
+++ app.js
@@ -0,0 +1,32 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const accountCreate=require('./routes/createAccount')
+const app=express()
+const port=3000
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const url = "mongodb+srv://facebook:yZh4ZcrsGGR9stcf@cluster0.wzywxdl.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+mongoose.connect(url,{useNewUrlParser:true})
+const con=mongoose.connection
+
+con.on("open",()=>{
+    console.log("Mongodb Connected");
+
+})
+app.use('/account',accountCreate)
+
+
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}`)
+})
